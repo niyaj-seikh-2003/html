d4a014763b7c7271bf4fa812b02ad3b528a55b78
niyaj-seikh-2003 <niyazshkk@gmail.com>
2025-10-14 10:39:19 +0530

    js practice

---
+++ javascript/js practice/script.js
@@ -0,0 +1,133 @@
+// console.log("Name = Niyaj Seikh")
+// console.log("Roll no is =101")
+// console.log("Branch =BSc")
+// console.log("Address =Odisha")
+// console.log("College Name is =Naresh It")
+
+
+
+// let a =20;
+// let b= 10;
+// console.log(`Addition is :${a+b}`)
+// console.log(`Substraction  is :${a-b}`)
+// console.log(`Multiplicatio is :${a*b}`)
+// console.log(`Division is :${a/b}`)
+// console.log(`Modulation is :${a%b}`)
+
+
+// let a = 10;
+// let b= 20;
+
+// console.log (`Addition is :${a-(-b)}`)
+
+
+// let num=+prompt("Enter a value");
+
+// console.log(Math.floor((num/10)+(num%10)))
+
+// let num=+prompt("Enter a value");
+
+// console.log(Math.floor((num/100)+(num%10)));
+
+// let square=+prompt("enter square")
+// console.log(`The aea of the square is ${square} is : ${square*square}`)
+
+// let length=+prompt()
+// let breadth=+prompt()
+// console.log(`the area of the rectangel with length ${length} and breadth ${breadth} is :${length*breadth}`)
+
+
+// let a=10;
+// let b=20;
+// let c;
+// console.log(`before swapping :a=${a},b=${b}`)
+
+// c=a,a=b,b=c
+// console.log(`after swapping :a=${a},b=${b}`)
+
+
+// let a=10,b=20;
+
+// console.log(`before swapping :a=${a},b=${b}`)
+
+// a=a+b;
+// b=a-b;
+// a=a-b;
+// console.log(`after swapping :a=${a},b=${b}`)
+
+
+// let a=26;
+// console.log(Math.floor(a/10)*10)
+
+// let b=34;
+// console.log(Math.floor(b/10+1)*10)
+
+
+
+// let num=+prompt()
+
+// num%2==0 || console.log("odd");
+// num%2!=0 || console.log("Even")
+
+
+
+// let num1=+prompt()
+// let num2=+prompt()
+// num1>num2 || console.log(`max=${num2}`)
+// num2>num1 || console.log(`max=${num1}`)
+
+
+
+
+// let num1=+prompt()
+// let num2=+prompt()
+// let num3=+prompt()
+// num1>num2 || console.log(`max=${num2}`)
+// num2>num3 || console.log(`max=${num3}`)
+// num3>num1 || console.log(`max=${num1}`);
+
+
+
+
+// let a=+prompt()
+// let b=+prompt()
+// let c=+prompt()
+
+// a>b&&a>c?console.log(`max=${a}`):b>a && b>c ?console.log(`max=${b}`):console.log(`max=${c}`)
+    
+
+
+
+// let a=+prompt()
+
+// a%2==0?console.log("Even"):console.log("Odd")
+
+
+
+let cost=3500;
+pro=27;
+vat=12.7;
+service=3.87;
+
+selling=cost+(cost*27)/100;
+profit=selling-cost;
+vat_value=(selling*12.7)/100;
+serviceCharge=(selling*3.87)/100;
+
+console.log(`cost price RS.${cost.toFixed(2)}`);
+console.log(`profit(${pro}%):RS.${profit.toFixed(2)}`);
+console.log(`selling price (without vat & setvice charge ):RS.${selling.toFixed(2)}`);
+console.log(`vat${(vat)}%:RS.${vat_value.toFixed(2)}`);
+console.log(`service charge ${(service)}% :RS.${serviceCharge.toFixed(2)}`);
+console.log(`totalselling price (with VAT & serviece charge):RS.${(selling+vat_value+serviceCharge).toFixed(2)}`)
+
+
+
+
+
+
+
+
+
+
+
